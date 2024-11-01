677ede588f0df3c6de9b805881bd67f8df054d46
NannaMiaHansen <10643801@ucn.dk>
2024-11-01 11:15:01 +0100

    Implemented FriendController
    I

--- application/FriendController.java
+++ application/FriendController.java
@@ -1,20 +1,31 @@
 package application;
+import model.FriendContainer;
+import model.Friend;
+
 
 
 /**
  * Write a description of class FriendController here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Nanna Mia Hansen & Lumière Schack)
+ * @version (01.11.2024 kl. 11.13)
  */
 public class FriendController
 {
+    private FriendContainer friendContainer;
+    private Friend friend;
     
     /**
      * Constructor for objects of class FriendController
      */
     public FriendController()
     {
+        friendContainer = FriendContainer.getInstance();
         
+    }
+    
+    public Friend findFriendByPhoneNumber (String phoneNumber)
+    {
+        return friendContainer.findFriendByPhoneNumber (phoneNumber);
     }
 }
