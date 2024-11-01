1a18ee155170e57ad329ba41bca4c21be0852c22
NannaMiaHansen <10643801@ucn.dk>
2024-11-01 10:24:34 +0100

    Friend
    Friend & FriendContainer

--- Model/Friend.java
+++ Model/Friend.java
@@ -1,20 +1,34 @@
-package model;
+package Model;
 
 
 /**
  * Write a description of class Friend here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Nanna Mia Hansen & Lumière Schack)
+ * @version (01.11.2024 kl. 9.17)
  */
 public class Friend
-{
+{   private String name;
+    private String address;
+    private int postalCode;
+    private String city;
+    private String phoneNumber;
     
     /**
      * Constructor for objects of class Friend
      */
-    public Friend()
+    public Friend(String name, String address, int postalCode, String city,
+                String phoneNumber)
     {
-        
+        this.name = name;
+        this.address = address;
+        this.postalCode = postalCode;
+        this.city = city;
+        this.phoneNumber = phoneNumber;
+    }
+    
+    public String getPhoneNumber()
+    {
+        return phoneNumber;
     }
 }
--- Model/FriendContainer.java
+++ Model/FriendContainer.java
@@ -1,20 +1,62 @@
-package model;
+package Model;
+import java.util.ArrayList;
 
 
 /**
  * Write a description of class FriendContainer here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Nanna Mia Hansen & Lumière Schack)
+ * @version (01.11.2024 kl. 9.42)
  */
 public class FriendContainer
 {
+    private ArrayList<Friend> friendCollection;
+    private static FriendContainer instance;
     
     /**
      * Constructor for objects of class FriendContainer
      */
-    public FriendContainer()
+    private FriendContainer()
     {
+        friendCollection = new ArrayList<>();
         
+    }
+    
+    public static FriendContainer getInstance()
+    {
+        if(instance == null) 
+        {
+              instance = new FriendContainer();
+        }
+        return instance;
+    }
+    
+    public Friend findFriendByPhoneNumber (String phoneNumber)
+    {
+        int index = 0;
+        boolean searching = true;
+        
+        while(searching && index < friendCollection.size())
+        {
+            Friend currentFriend = friendCollection.get(index);
+            String friendPhoneNumber = currentFriend.getPhoneNumber();
+            if(friendPhoneNumber.equals(phoneNumber))
+            {
+                searching = false;
+            }
+            else
+            {
+                index = index + 1;
+            }
+        }
+        
+        if(searching)
+        {
+            return null;
+        }
+        else
+        {
+            return friendCollection.get(index);
+        }
     }
 }
